76e69e2a766414bbd7b319854584dd794b26d6cd
Manuel Ernesto <manuelernesto@outlook.com>
2016-06-09 17:52:56 +0100

    Terminei com o projecto de ensinar um CRUD com SQLite no Android

--- app/src/main/java/ao/studio/me/meuscontactos/ListaActivity.java
+++ app/src/main/java/ao/studio/me/meuscontactos/ListaActivity.java
@@ -3,6 +3,8 @@ package ao.studio.me.meuscontactos;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -25,7 +27,7 @@ public class ListaActivity extends AppCompatActivity {
                 Contacto contacto = (Contacto) parent.getItemAtPosition(position);
                 Intent i = new Intent(ListaActivity.this, MainActivity.class);
                 i.putExtra("c",contacto);
-                    startActivity(i);
+                startActivity(i);
             }
         });
 
@@ -39,11 +41,36 @@ public class ListaActivity extends AppCompatActivity {
                                 contactos);
 
         listView.setAdapter(adapter);
+        registerForContextMenu(listView);
 
     }
 
     public void cadastro(View view){
         Intent i = new Intent(ListaActivity.this,MainActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if (v.equals(listView)){
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            Contacto contacto = (Contacto) listView.getItemAtPosition(info.position);
+            criarMenu(menu, contacto);
+
+        }
+    }
+
+    public void criarMenu(ContextMenu menu, final Contacto contacto){
+        MenuItem menuItem = menu.add("Apagar");
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                ContactoDAO dao = new ContactoDAO(ListaActivity.this);
+                dao.apagar(contacto);
+                Intent i = new Intent(ListaActivity.this,ListaActivity.class);
+                startActivity(i);
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/ao/studio/me/meuscontactos/MainActivity.java
+++ app/src/main/java/ao/studio/me/meuscontactos/MainActivity.java
@@ -1,9 +1,8 @@
 package ao.studio.me.meuscontactos;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,7 +26,13 @@ public class MainActivity extends AppCompatActivity {
         emailEd = (EditText) findViewById(R.id.email);
 
 
-
+        if (existeContcto()) {
+            Contacto contacto = contactoParaActualizar();
+            nomeEd.setText(contacto.getNome());
+            telefoneEd.setText(contacto.getTelefone());
+            emailEd.setText(contacto.getEmail());
+            btnSalvar.setText("Actualizar");
+        }
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,9 +40,15 @@ public class MainActivity extends AppCompatActivity {
                 Contacto contacto = new Contacto(nomeEd.getText().toString(),
                         telefoneEd.getText().toString(),
                         emailEd.getText().toString());
+                if (existeContcto()) {
+                    dao.actualizar(contacto, contactoParaActualizar().getId());
+                    Toast.makeText(MainActivity.this, "Contacto Actualizado com sucesso!", Toast.LENGTH_SHORT).show();
 
-                dao.salvar(contacto);
-                Toast.makeText(MainActivity.this, "Contacto Salvo com sucesso!",Toast.LENGTH_SHORT).show();
+                } else {
+                    dao.salvar(contacto);
+                    Toast.makeText(MainActivity.this, "Contacto Salvo com sucesso!", Toast.LENGTH_SHORT).show();
+
+                }
                 nomeEd.setText("");
                 telefoneEd.setText("");
                 emailEd.setText("");
@@ -46,12 +57,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void listar(View view){
+    public void listar(View view) {
         Intent i = new Intent(MainActivity.this, ListaActivity.class);
         startActivity(i);
     }
 
+    public boolean existeContcto() {
+        return getIntent().hasExtra("c");
+    }
 
+    public Contacto contactoParaActualizar() {
+        Intent intent = getIntent();
+        Contacto contacto = (Contacto) intent.getSerializableExtra("c");
+        return contacto;
+    }
 
 
 }
